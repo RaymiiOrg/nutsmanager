0eb3a57e40845a7debe4fd0ce912e2038773e15a
Remy <relst@relst.nl>
2015-04-05 21:09:58 +0200

    use local libs

--- header.php
+++ header.php
@@ -26,17 +26,15 @@ $productversion = "v0.0.4";
 
 <!DOCTYPE html>
 <html><head>
-<title><?php echo $productname; ?> - Raymii.org</title>
+<title><?php echo $productname; ?></title>
 <meta charset="UTF-8">
 <meta name="description" content="" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<!--<script type="text/javascript" src="js/jquery.min.js"></script>-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.flot.js"></script>                                   
-<!--<script type="text/javascript" src="js/prettify.js"></script>  -->
-<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/cerulean/bootstrap.min.css" rel="stylesheet">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
 </head>
 <a id="top-of-page"></a>
   <div id='page-content-wrapper'>
